488d23c1fd50fc84a840e7d13379fe11d9b2cb74
Harsh Pandey <harshpandey1306@gmail.com>
2025-07-21 22:09:05 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -139,7 +139,33 @@ const About = () => {
                     <polyline points="7 10 12 15 17 10"></polyline>
                     <line x1="12" y1="15" x2="12" y2="3"></line>
                   </svg>
-                  Download Resume
+                  Download Resume (India)
+                </Button>
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1fQQwv3z2gMRyt8L1ObosawdkA4IY-Qvl/view?usp=sharing"
+                download
+                className="inline-flex items-center justify-center"
+                target="_blank"
+              >
+                <Button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2"
+                  >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                    <polyline points="7 10 12 15 17 10"></polyline>
+                    <line x1="12" y1="15" x2="12" y2="3"></line>
+                  </svg>
+                  Download Resume (Singapore)
                 </Button>
               </a>
               <a href="mailto:harshpandeywork13@gmail.com">
